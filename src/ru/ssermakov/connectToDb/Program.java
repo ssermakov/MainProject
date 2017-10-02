84df08392dde00d10f102b1796ecbc6f679bb956
ssermakov <btbwild.ermakov@gmail.com>
2017-10-02 15:18:19 +1200

    initinitial commit

---
+++ src/ru/ssermakov/connectToDb/Program.java
@@ -0,0 +1,38 @@
+package ru.ssermakov.connectToDb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Program {
+	
+	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/zumanaha?user=root&password=";
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName(DRIVER_NAME);
+		System.out.println("test");
+		Connection conn = DriverManager.getConnection(CONNECTION_STRING);
+		
+		Scanner sc = new Scanner (System.in);
+		String line = sc.nextLine();
+		
+		String sql = "SELECT wo FROM return_objects";
+		PreparedStatement cmd = (PreparedStatement) conn.prepareStatement(sql);
+		cmd.setString(1, "%"+line+"%");
+		ResultSet res = cmd.executeQuery();
+		System.out.println("test");
+		while (res.next()) {
+			String wo = res.getString("wo");
+			
+			System.out.printf("%s", wo);
+		}
+		
+		res.close();
+		conn.close();
+	}
+
+}
